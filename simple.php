6c674d7433fbe6b00618aeaedf70e4e52e43e6f4
Thanawat <thanawat_ben@hotmail.com>
2017-03-20 11:05:34 +0700

    -

--- simple.php
+++ simple.php
@@ -7,8 +7,6 @@
     </head>
     <body>
 
-        <p>Lorem <b>ipsum</b> dolor sit amet, consectetur adipiscing elit. Sed eu arcu porta, ultrices massa ut, porttitor diam. Integer id auctor augue.</p>
-        <p>Vivamus mattis, sem id consequat dapibus, odio urna fermentum risus, in blandit dolor justo vel ex. Curabitur a neque bibendum, hendrerit sem in, congue lectus.</p>
-
+        <h1>Hello</h1>
     </body>
 </html>
