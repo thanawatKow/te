4535c72bf4a04a602dc225e97ab6b35bd8cd5b10
Thanawat <thanawat_ben@hotmail.com>
2017-03-20 11:00:08 +0700

    -

--- 1.php
+++ 1.php
@@ -21,31 +21,6 @@ $transformer->transform($instant_article, $document);
 $f = fopen('simple_test.php', 'w') or die("can't open file");
 fwrite($f,$result);
 //echo $result;
-// @TODO: Replace the data with your own information
-    $app_id = "1335396193222019";
-    $app_secret = "45cb6b14299eafb282bdf42f4315ea33";
-    $page_access_token = "EAACEdEose0cBAGnIank3mTDPNyZC9hYFwyF3bYurZBvLMegoGLpQE2Xol08M8HrQx5WhxzcaAStk8ZCysaeqY9VTAb31RVT1XrfF9o9rfzIkwHj2lidn5hBtWSbdk1sjgZBVtnDZCXZBvCZAt0jvIZBfW0nvDvaB2l4QlBcWZArZAiaE18nJr9Cyyza6uWWfjM0AIZD";
-    $page_id = "1920957664803342";
-    $dev_mode =  False; // False for production, true for development mode
-    $take_live = false; // Says to send to live or save as draft
 
-    // Instantiate an API client.
-    try {
-
-      $client = Client::create(
-        $app_id,
-        $app_secret,
-        $page_access_token,
-        $page_id,
-        $dev_mode
-      );
-
-      // Import the article into Facebook
-
-
-    } catch ( Exception $e ) {
-      // @TODO: Treat the error properly here.
-    }
-   $client->importArticle( $instant_article,true);
 
 ?>
